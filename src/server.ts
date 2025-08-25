2952790f680b692dd70755982c0e1355d67521d1
ALIF <sayedalif18@gmail.com>
2025-08-26 02:55:35 +0600

    server and project eslint setup

---
+++ src/server.ts
@@ -0,0 +1,72 @@
+/* eslint-disable no-console */
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+
+let server: Server;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/rideBookingSystem');
+    console.log('Connected To DB!!');
+
+    server = app.listen(5000, () => {
+      console.log('Server is listening on port 5000');
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received, Shutting down the server...');
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  // if no server is detected, but SIGTERM occurs then also shut down the server
+  process.exit(1);
+});
+
+process.on('SIGINT', () => {
+  console.log('SIGINT signal received, Shutting down the server...');
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  // if no server is detected, but SIGINT occurs then also shut down the server
+  process.exit(1);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('Unhandled Rejection Error! Shutting down the server...', err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  // if no server is detected, but some unhandled rejection occurs then also shut down the server
+  process.exit(1);
+});
+
+process.on('uncaughtException', err => {
+  console.log('Uncaught Exception Error! Shutting down the server...', err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  // if no server is detected, but some uncaught exception occurs then also shut down the server
+  process.exit(1);
+});
+
